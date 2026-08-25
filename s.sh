eb971ffdfb9a9cb1027d4fad8436b5441977e480
Liquid <13621351+liqtags@users.noreply.github.com>
2026-08-25 15:55:04 +0100

    Update wallet count to 1000 in index.json and README.md; refactor s.sh to use a loop for deno commands

--- s.sh
+++ s.sh
@@ -1,5 +1,11 @@
-deno run --allow-all main.ts data-0-300_000/data-0-300_000.json 0 100
-deno run --allow-all main.ts data-0-300_000/data-0-300_000.json 100 200
-deno run --allow-all main.ts data-0-300_000/data-0-300_000.json 200 300
-deno run --allow-all main.ts data-0-300_000/data-0-300_000.json 300 400
-deno run --allow-all main.ts data-0-300_000/data-0-300_000.json 400 500
+# deno run --allow-all main.ts data-0-300_000/data-0-300_000.json 0 100
+# deno run --allow-all main.ts data-0-300_000/data-0-300_000.json 100 200
+# deno run --allow-all main.ts data-0-300_000/data-0-300_000.json 200 300
+# deno run --allow-all main.ts data-0-300_000/data-0-300_000.json 300 400
+# deno run --allow-all main.ts data-0-300_000/data-0-300_000.json 400 500
+for ((i=500; i<1000; i+=100))
+do
+  start=$i
+  end=$((i+100))
+  deno run --allow-all main.ts data-0-300_000/data-0-300_000.json $start $end
+done
